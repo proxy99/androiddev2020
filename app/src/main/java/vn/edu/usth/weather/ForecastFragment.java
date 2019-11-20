739d21811eb31736a3ca7c44e4080e9d4cd4b034
proxy99 <tanganhquan1999@gmail.com>
2019-11-20 16:56:47 +0700

    update labwork4

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,11 +1,11 @@
 package vn.edu.usth.weather;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +14,15 @@ import android.view.ViewGroup;
 public class ForecastFragment extends Fragment {
     public ForecastFragment() {
     }
-
+//  Labwork 3 here
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//    Inflate layout
+//    View v = new View(getContext());
+//    v.setBackgroundColor(0x00FFFF);
+//    return v;}
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate layout
-        View v = new View(getContext());
-        v.setBackgroundColor(0x00FFFF);
-        return v;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState){
+        return inflater.inflate(R.layout.fragment_forecast,container,false);
     }
 }
