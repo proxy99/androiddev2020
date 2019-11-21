8845d0603edb16bde0798de28fde535263fc0dc4
proxy99 <tanganhquan1999@gmail.com>
2019-11-21 20:37:46 +0700

    finish labwork 6

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,13 +1,18 @@
 package vn.edu.usth.weather;
 
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class ForecastFragment extends Fragment {
